d7a0570ce2006c98f001b39b2702fcfb9f48f5d1
Be_a_panda <huayusheng0912@gmail.com>
2020-04-14 20:57:20 -0500

    debug

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -8961,7 +8961,8 @@ void OSD::dequeue_op(
 	   << " " << *(op->get_req())
 	   << " pg " << *pg << dendl;
 
-  dout(1) << "#wait_for_servie = " << latency << "#"<< dendl;
+  if((op->get_req()->get_type()) == MSG_OSD_EC_READ)
+    dout(1) << "#wait_for_servie = " << latency << "#"<< dendl;
   // share our map with sender, if they're old
   if (op->send_map_update) {
     Message *m = op->get_req();
