1ca9ae00e7389aca3c22157fd978fcbd562b8356
Be_a_panda <huayusheng0912@gmail.com>
2020-05-22 03:04:50 -0500

    debug

--- src/osd/ECBackend.cc
+++ src/osd/ECBackend.cc
@@ -2164,12 +2164,13 @@ void ECBackend::start_read_op(
 						
 					}else if(osd->whoami==(osd->pipeline_length-1)){//如果是最后一个，先订阅开始信号，接着直接开始osd->osd_num-1
 						dout(1)<< ": mydebug: in OSD2!" << dendl;
-						if(osd->subscribe(osd->subscribe_channel[(j==0?8:j)-1],start_msg)){
+						if(osd->subscribe(osd->subscribe_channel[j],start_msg)){
 							dout(1)<< ": mydebug: subscribe finish!" << dendl;
 						}
 						osd->redis_lock(std::string("OSD")+std::to_string(j));
 						while(!osd->sending_queue_list[j].osd_queue.empty()){
 							OSDService::queue_element &first_element = osd->sending_queue_list[j].osd_queue.front();
+							first_element.msg->op.batch_size = length;
 							first_element.msg->op.send_time = ceph_clock_now(cct);
 							first_element.listener->send_message_osd_cluster(
 								first_element.osd,
@@ -2181,11 +2182,12 @@ void ECBackend::start_read_op(
 						}
 					}else{//中间节点，先等待开始信号，发送完之后再接着发送开始信号给下一个
 						dout(1)<< ": mydebug: in OSD1!" << dendl;
-						if(osd->subscribe(osd->subscribe_channel[(j==0?8:j)-1],start_msg)){
+						if(osd->subscribe(osd->subscribe_channel[j],start_msg)){
 							dout(1)<< ": mydebug: subscribe finish!" << dendl;
 							osd->redis_lock(std::string("OSD")+std::to_string(j));
 							while(!osd->sending_queue_list[j].osd_queue.empty()){
 								OSDService::queue_element &first_element = osd->sending_queue_list[j].osd_queue.front();
+								first_element.msg->op.batch_size = length;
 								first_element.msg->op.send_time = ceph_clock_now(cct);
 								first_element.listener->send_message_osd_cluster(
 									first_element.osd,
